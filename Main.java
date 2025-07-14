5534c3d73ca76e521c68d5c7f386a2ad94c30afb
Mauro-Suesca <msuesca@unal.edu.co>
2025-07-14 13:08:17 -0500

    [Main.java] Created main class and main method for starting the game

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        Board board = new Board(12, 6, 6);
+
+        //TODO Add the Cells and lines to the Board
+        
+        //TODO Set the first step as the vertical line with 0 mines.
+
+        board.render();
+        
+    }
+}
