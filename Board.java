5d7e927d6df4480f04d0983c1d38d335c535ba17
Mauro-Suesca <msuesca@unal.edu.co>
2025-07-18 14:12:39 -0500

    [Board.java] Changed the way the board displays itself

--- Board.java
+++ Board.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class Board implements CellObserver {
+public class Board implements CellObserver{
     private Cell[][] cellsInBoard;
     private ExternalCounter[] verticalLines;
     private ExternalCounter[] horizontalLines;
@@ -11,7 +11,7 @@ public class Board implements CellObserver {
     private int currentColumn;
     private Scanner waitForUserInput;
 
-    Board(int numberOfMinesInBoard, int columns, int rows) {
+    Board(int numberOfMinesInBoard, int columns, int rows){
         this.totalMineCounter = new ExternalCounter(numberOfMinesInBoard);
         this.cellsInBoard = new Cell[columns][rows];
         this.verticalLines = new ExternalCounter[columns];
@@ -21,65 +21,65 @@ public class Board implements CellObserver {
         resetCurrentRowAndColumn();
     }
 
-    public void addCell(Cell newCell) {
+    public void addCell(Cell newCell){
         cellsInBoard[currentRow][currentColumn] = newCell;
 
-        if (++currentColumn == verticalLines.length) {
+        if (++currentColumn == verticalLines.length){
             currentColumn = 0;
             currentRow++;
         }
     }
 
-    public void addVerticalLine(ExternalCounter newLine) {
+    public void addVerticalLine(ExternalCounter newLine){
         verticalLines[currentColumn++] = newLine;
     }
 
-    public void addHorizontalLine(ExternalCounter newLine) {
+    public void addHorizontalLine(ExternalCounter newLine){
         horizontalLines[currentRow++] = newLine;
     }
 
-    public void resetCurrentRowAndColumn() {
+    public void resetCurrentRowAndColumn(){
         currentRow = 0;
         currentColumn = 0;
     }
 
-    public void addCellToProcess(EmptyCell cellToAdd) {
+    public void addCellToProcess(EmptyCell cellToAdd){
         cellsToProcess.add(cellToAdd);
     }
 
-    public void setFirstStep(EmptyCell firstCellToProcess) {
+    public void setFirstStep(EmptyCell firstCellToProcess){
         if (cellsToProcess.size() == 0) {
             cellsToProcess.add(firstCellToProcess);
         }
     }
 
-    public void executeNextProcess() {
-        if (!cellsToProcess.isEmpty()) {
+    public void executeNextProcess(){
+        if (!cellsToProcess.isEmpty()){
             cellsToProcess.remove().executeLogicalSequence();
             executeNextProcess();
         }
     }
 
     @Override
-    public void reactToCellReveal(Cell revealedCell) {
+    public void reactToCellReveal(Cell revealedCell){
         render();
     }
 
     @Override
-    public void reactToCellMarked(Cell markedCell) {
+    public void reactToCellMarked(Cell markedCell){
         render();
     }
 
     public void render() {
         clearScreen();
 
-        System.out.print("   ");
-        for (ExternalCounter line : verticalLines) {
+        System.out.print("  ");
+        for (ExternalCounter line : verticalLines){
             System.out.print(line);
         }
         System.out.println();
 
-        for (int row = 0; row < cellsInBoard.length; row++) {
+        for (int row = 0; row < cellsInBoard.length; row++){
             System.out.print(horizontalLines[row]);
             for (int column = 0; column < cellsInBoard[row].length; column++) {
                 System.out.print(cellsInBoard[row][column]);
@@ -87,9 +87,12 @@ public class Board implements CellObserver {
             System.out.println();
         }
 
-        if (totalMineCounter.getRemainingNumberOfAdjacencies() == 0) {
+        if (totalMineCounter.getRemainingNumberOfAdjacencies() == 0){
             System.out.println("\nCongratulations, the level has been completed!");
             System.out.print("\nPress 'ENTER' to finish");
+            waitForUserInput.nextLine();
+
+            clearScreen();
             waitForUserInput.close();
             return;
         }
@@ -99,10 +102,10 @@ public class Board implements CellObserver {
     }
 
     // Only works for Tametsi level 9, could be useful to check other ways for other levels
-    public void autoAdjacencySetter() {
+    public void autoAdjacencySetter(){
         // Cell and Board to Cell adjacency
-        for (int row = 0; row < cellsInBoard.length; row++) {
-            for (int column = 0; column < cellsInBoard[row].length; column++) {
+        for (int row = 0; row < cellsInBoard.length; row++){
+            for (int column = 0; column < cellsInBoard[row].length; column++){
                 Cell currentCell = cellsInBoard[row][column];
 
                 addIfValidPosition(currentCell, row - 1, column - 1);
@@ -122,8 +125,8 @@ public class Board implements CellObserver {
         }
 
         // Cell and Board to Line adjacency
-        for (int row = 0; row < cellsInBoard.length; row++) {
-            for (int column = 0; column < cellsInBoard[row].length; column++) {
+        for (int row = 0; row < cellsInBoard.length; row++){
+            for (int column = 0; column < cellsInBoard[row].length; column++){
                 verticalLines[column].addAdjacent(cellsInBoard[row][column]);
                 horizontalLines[row].addAdjacent(cellsInBoard[row][column]);
 
@@ -137,7 +140,7 @@ public class Board implements CellObserver {
         totalMineCounter.addBoard(this);
     }
 
-    private void addIfValidPosition(Cell currentCell, int row, int column) {
+    private void addIfValidPosition(Cell currentCell, int row, int column){
         if (row < 0 || row >= horizontalLines.length) {
             return;
         }
@@ -148,8 +151,11 @@ public class Board implements CellObserver {
         currentCell.addAdjacent(cellsInBoard[row][column]);
     }
 
-    private void clearScreen() {
-        System.out.print("\033[H\033[2J"); //ANSI Code for clearing the screen
+    private void clearScreen(){
+        for(int i=0; i<2; i++){
+            System.out.print("\u001B[3J\u001B[2J\u001B[H\u001B[0m"); //ANSI Code for clearing the screen
+        }
+        
         System.out.flush();
     }
 }
