876e2840519cbfbc83df337a1863f3e3ab0736a1
Mauro-Suesca <msuesca@unal.edu.co>
2025-08-21 19:30:13 -0500

    Applied a band-aid to the issue in the previous commit, because alternate solutions didn't seem to work

--- src/ImaginaryCell.java
+++ src/ImaginaryCell.java
@@ -1,21 +1,48 @@
 import java.util.ArrayList;
 
 public class ImaginaryCell extends EmptyCell{
+    private ArrayList<Cell> originalAdjacencies;
+
     ImaginaryCell(int remainingMines, ArrayList<Cell> remainingAdjacentCells){
         super(remainingMines, true, false);
-        this.remainingAdjacentCells = remainingAdjacentCells;
+        this.originalAdjacencies = remainingAdjacentCells;
     }
 
     public void addToBoard(Board board){
         this.addBoard(board);
-        ArrayList<Cell> copyOfRemainingAdjacentCells = new ArrayList<>(remainingAdjacentCells);
 
         this.remainingAdjacentCells = new ArrayList<>();
 
-        for(Cell cell : copyOfRemainingAdjacentCells){
+        for(Cell cell : originalAdjacencies){
             this.addAdjacent(cell);
         }
 
         this.board.addCellToProcess(this);
+    }
+
+    @Override public boolean equals(Object otherObject){
+        if(otherObject instanceof ImaginaryCell){
+            ImaginaryCell otherCell = (ImaginaryCell)otherObject;
+
+
+            if(otherCell.originalAdjacencies.size() != this.originalAdjacencies.size()){
+                return false;
+            }
+
+            //I have no clue why, but if I replace this try catch with a check to see if both lists have the same size, the code becomes SLOWER, so I'm leaving it in
+            try{
+                for(int i=0; i<this.remainingAdjacentCells.size(); i++){
+                    if(otherCell.remainingAdjacentCells.get(i) != this.remainingAdjacentCells.get(i)){
+                        return false;
+                    }
+                }
+            }catch(IndexOutOfBoundsException e){
+                return false;
+            }
+
+            return true;
+        }
+        
+        return false;
     }
 }
