3eee439a6cb8fca54f7a9500b22f96882dbdb9f0
Mauro-Suesca <msuesca@unal.edu.co>
2026-01-05 21:11:48 -0500

    [README.md] Added level 48 to the solved levels

---
+++ src/levels/Level48.java
@@ -0,0 +1,230 @@
+package levels;
+
+import cellFactories.EmptyCellFactory;
+import cellFactories.ExternalCounterFactory;
+import cellFactories.MineCellFactory;
+import cellFactories.NonExistentCellFactory;
+import cellLogic.Board;
+import cellLogic.BoardWithLines;
+import cellLogic.ColorCounter;
+import cellLogic.EmptyCell;
+import cellLogic.MineCell;
+
+public class Level48 extends Level{
+    @Override protected Board setup(){
+        ColorCounter gray = new ColorCounter(48, "grises", "\u001B[0m");
+        ColorCounter cyan = new ColorCounter(1, "cian", "\u001B[36m");
+        ColorCounter green = new ColorCounter(3, "verdes", "\u001B[32m");
+        ColorCounter yellow = new ColorCounter(4, "amarillas", "\u001B[33m");
+        ColorCounter pink = new ColorCounter(5, "rosadas", "\u001B[38;5;212m");
+
+        BoardWithLines board = new BoardWithLines(16, 16, false, gray, cyan, green, yellow, pink);
+
+        ExternalCounterFactory externalCounterFactory = new ExternalCounterFactory();
+        NonExistentCellFactory nonExistentCellFactory = new NonExistentCellFactory();
+
+        MineCellFactory grayMineFactory = new MineCellFactory(gray);
+        MineCellFactory yellow2x2MineFactory = new MineCellFactory(yellow, 2, 2);
+                
+        EmptyCellFactory grayUnrevealedEmptyFactory = new EmptyCellFactory(gray, false, false);
+        EmptyCellFactory cyanUnrevealedEmptyFactory = new EmptyCellFactory(cyan, false, false);
+        EmptyCellFactory greenUnrevealedEmptyFactory = new EmptyCellFactory(green, false, false);
+        EmptyCellFactory yellowUnrevealedEmptyFactory = new EmptyCellFactory(yellow, false, false);
+        EmptyCellFactory pinkUnrevealedEmptyFactory = new EmptyCellFactory(pink, false, false);
+
+        EmptyCellFactory yellow2x2EmptyFactory = new EmptyCellFactory(yellow, false, false, 2, 2);
+        EmptyCellFactory cyan2x2EmptyFactory = new EmptyCellFactory(cyan, false, false, 2, 2);
+        EmptyCellFactory green2x2EmptyFactory = new EmptyCellFactory(green, false, false, 2, 2);
+        EmptyCellFactory pink2x2EmptyFactory = new EmptyCellFactory(pink, false, false, 2, 2);
+
+        //Line 1
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(9));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(grayMineFactory.build(1));
+
+        //Line 2
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(3));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(new EmptyCell(gray, false, true));
+        board.addCell(grayMineFactory.build(2));
+        board.addCell(grayUnrevealedEmptyFactory.build(5));
+        board.addCell(grayMineFactory.build(1));
+
+        //Line 3
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(cyan2x2EmptyFactory.build(1));
+        board.addCell(new EmptyCell(cyan, false, true));
+        board.addCell(cyanUnrevealedEmptyFactory.build(2));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(green2x2EmptyFactory.build(1));
+        board.addCell(new MineCell(green));
+        board.addCell(new MineCell(green, 2, 2));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+
+        //Line 4
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(cyan2x2EmptyFactory.build(1));
+        board.addCell(cyanUnrevealedEmptyFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(greenUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+
+        //Line 5
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(cyanUnrevealedEmptyFactory.build(2));
+        board.addCell(new MineCell(cyan));
+        board.addCell(new EmptyCell(gray, false, true));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(greenUnrevealedEmptyFactory.build(1));
+        board.addCell(new MineCell(green));
+        board.addCell(green2x2EmptyFactory.build(1));
+        board.addCell(greenUnrevealedEmptyFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+
+        //Line 6
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(cyan2x2EmptyFactory.build(1));
+        board.addCell(cyanUnrevealedEmptyFactory.build(1));
+        board.addCell(cyan2x2EmptyFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(green2x2EmptyFactory.build(1));
+        board.addCell(greenUnrevealedEmptyFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+
+        //Line 7
+        board.addCell(grayMineFactory.build(2));
+        board.addCell(cyanUnrevealedEmptyFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(greenUnrevealedEmptyFactory.build(3));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+
+        //Line 8
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(grayMineFactory.build(2));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(6));
+        board.addCell(grayMineFactory.build(2));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+
+        //Line 9
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(grayMineFactory.build(2));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(new EmptyCell(gray, false, true));
+        board.addCell(grayUnrevealedEmptyFactory.build(4));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(new EmptyCell(gray, false, true));
+
+        //Line 10
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(yellowUnrevealedEmptyFactory.build(3));
+        board.addCell(yellow2x2MineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(new MineCell(pink, 2, 2));
+        board.addCell(pinkUnrevealedEmptyFactory.build(1));
+        board.addCell(pink2x2EmptyFactory.build(1));
+        board.addCell(new EmptyCell(gray, false, true));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+
+        //Line 11
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(new MineCell(yellow));
+        board.addCell(yellow2x2EmptyFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(pinkUnrevealedEmptyFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+
+        //Line 12
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(new MineCell(yellow));
+        board.addCell(yellowUnrevealedEmptyFactory.build(2));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(new MineCell(pink));
+        board.addCell(pink2x2EmptyFactory.build(1));
+        board.addCell(pinkUnrevealedEmptyFactory.build(2));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+
+        //Line 13
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(yellow2x2EmptyFactory.build(1));
+        board.addCell(yellowUnrevealedEmptyFactory.build(1));
+        board.addCell(yellow2x2MineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(new MineCell(pink));
+        board.addCell(pink2x2EmptyFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+
+        //Line 14
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(yellowUnrevealedEmptyFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(new MineCell(pink));
+        board.addCell(pinkUnrevealedEmptyFactory.build(1));
+        board.addCell(new MineCell(pink));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+
+        //Line 15
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(3));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(2));
+        board.addCell(new EmptyCell(gray, false, true));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(1));
+        board.addCell(grayUnrevealedEmptyFactory.build(1));
+        board.addCell(grayMineFactory.build(3));
+
+        //Line 16
+        board.addCell(grayUnrevealedEmptyFactory.build(16));
+
+        board.resetCurrentRowAndColumn();
+
+        //VerticalLines
+        board.addVerticalLine(externalCounterFactory.build(2));
+        board.addVerticalLine(nonExistentCellFactory.build(5));
+        board.addVerticalLine(externalCounterFactory.build(2));
+        board.addVerticalLine(nonExistentCellFactory.build(5));
+        board.addVerticalLine(externalCounterFactory.build(2));
+
+        //HorizontalLines
+        board.addHorizontalLine(externalCounterFactory.build(2));
+        board.addHorizontalLine(nonExistentCellFactory.build(5));
+        board.addHorizontalLine(externalCounterFactory.build(2));
+        board.addHorizontalLine(nonExistentCellFactory.build(5));
+        board.addHorizontalLine(externalCounterFactory.build(2));
+        
+        return board;
+    }
+
+    @Override public String toString(){
+        return "48";
+    }
+}
