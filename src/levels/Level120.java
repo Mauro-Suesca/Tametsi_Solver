4930a28e4c3e398f47f32aa4d071de20f217a8e2
Mauro-Suesca <msuesca@unal.edu.co>
2026-02-27 21:47:22 -0500

    [README.md] Added level 120 to the solved levels

---
+++ src/levels/Level120.java
@@ -0,0 +1,106 @@
+package levels;
+
+import cellFactories.CellFactory;
+import cellFactories.EmptyCellFactory;
+import cellFactories.ExternalCounterFactory;
+import cellFactories.MineCellFactory;
+import cellLogic.Board;
+import cellLogic.BoardWithLines;
+
+public class Level120 extends Level{
+    @Override protected Board setup(){
+        BoardWithLines board = new BoardWithLines(31, 9, 9, true);
+
+        board.setMaxMineDifferenceForImaginaryCells(5);
+
+        CellFactory.setBoard(board);
+
+        ExternalCounterFactory externalCounterFactory = new ExternalCounterFactory();
+
+        MineCellFactory mineFactory = new MineCellFactory();
+                
+        EmptyCellFactory unrevealedEmptyFactory = new EmptyCellFactory(false, false);
+        
+        EmptyCellFactory unknownFactory = new EmptyCellFactory(false, true);
+
+        //Row 1
+        unrevealedEmptyFactory.addToBoard(2);
+        unknownFactory.addToBoard(2);
+        unrevealedEmptyFactory.addToBoard(1);
+        mineFactory.addToBoard(1);
+        unrevealedEmptyFactory.addToBoard(3);
+        
+        //Row 2
+        unrevealedEmptyFactory.addToBoard(1);
+        mineFactory.addToBoard(2);
+        unrevealedEmptyFactory.addToBoard(1);
+        mineFactory.addToBoard(1);
+        unrevealedEmptyFactory.addToBoard(2);
+        mineFactory.addToBoard(2);
+
+        //Row 3
+        unrevealedEmptyFactory.addToBoard(5);
+        mineFactory.addToBoard(1);
+        unrevealedEmptyFactory.addToBoard(3);
+
+        board.setAddsCellsHorizontally(false);
+
+        //Column 1
+        mineFactory.addToBoard(4);
+        unrevealedEmptyFactory.addToBoard(1);
+        mineFactory.addToBoard(1);
+
+        //Column 2
+        unrevealedEmptyFactory.addToBoard(6);
+
+        //Column 3
+        unrevealedEmptyFactory.addToBoard(2);
+        mineFactory.addToBoard(3);
+        unrevealedEmptyFactory.addToBoard(1);
+
+        board.setAddsCellsHorizontally(true);
+
+        //Row 4
+        mineFactory.addToBoard(3);
+        unrevealedEmptyFactory.addToBoard(2);
+        mineFactory.addToBoard(1);
+
+        //Row 5
+        mineFactory.addToBoard(3);
+        unrevealedEmptyFactory.addToBoard(2);
+        mineFactory.addToBoard(1);
+
+        //Row 6
+        unrevealedEmptyFactory.addToBoard(4);
+        mineFactory.addToBoard(1);
+        unrevealedEmptyFactory.addToBoard(1);
+
+        //Row 7
+        unrevealedEmptyFactory.addToBoard(1);
+        mineFactory.addToBoard(2);
+        unrevealedEmptyFactory.addToBoard(3);
+
+        //Row 8
+        mineFactory.addToBoard(1);
+        unrevealedEmptyFactory.addToBoard(3);
+        mineFactory.addToBoard(2);
+
+        //Row 9
+        unrevealedEmptyFactory.addToBoard(4);
+        mineFactory.addToBoard(2);
+
+        board.resetCurrentRowAndColumn();
+
+        //VerticalLines
+        board.addVerticalLine(externalCounterFactory.build(9));
+
+        //HorizontalLines
+        board.addHorizontalLine(externalCounterFactory.build(9));
+
+        return board;
+    }
+
+    @Override public String toString(){
+        return "120";
+    }
+}
